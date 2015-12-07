e745286d8a20d0a1deedf09bfc6178c618d010ed
ShahabBakht <shahab.bakhtiari@gmail.com>
2015-12-07 18:05:35 -0500

    Added python file

--- ExploratoryAnalysis_BornEffect.m
+++ ExploratoryAnalysis_BornEffect.m
@@ -1,20 +1,25 @@
 %%
 
-X = X2602;
-Y = Y2602;
-X(:,21:40,:) = X2603;
-Y(:,21:40,:) = Y2603;
-X(:,41:60,:) = X2604(:,:,1:2914);
-Y(:,41:60,:) = Y2604(:,:,1:2914);
+X = X0701(:,:,1:2414);
+X(:,21:40,:) = X0702(:,:,1:2414);
+X(:,41:60,:) = X0703(:,:,1:2414);
+X(:,61:80,:) = X0704(:,:,1:2414);
 
-S = S2602;
-S(:,21:40,:) = S2603;
-S(:,41:60,:) = S2604;
+S = S0701;
+S(:,21:40,:) = S0702;
+S(:,41:60,:) = S0703;
+S(:,61:80,:) = S0704;
+
+TV = TV0701;
+TV(:,21:40) = TV0702;
+TV(:,41:60) = TV0703;
+TV(:,61:80) = TV0704;
+
 
 %%
 
-for c = 1:6
-    for tr = 1:60
+for c = 1:8
+    for tr = 1:80
         x = squeeze(X(c,tr,:));
         Send = floor(S(c,tr,3));
         Sstart = floor(S(c,tr,2));
@@ -22,17 +27,22 @@ for c = 1:6
         x_posS = x((Send + 40):(Send + 100));
         x_preS = x((Sstart - 100):(Sstart - 20));
         
-        y = squeeze(Y(c,tr,:));
-        y_posS = y((Send + 40):(Send + 100));
-        y_preS = y((Sstart - 100):(Sstart - 20));
+        v_posS = (x_posS(end) - x_posS(1))./0.06;
+        v_preS = (x_preS(end) - x_preS(1))./0.08;
         
+%         y = squeeze(Y(c,tr,:));
+%         y_posS = y((Send + 40):(Send + 100));
+%         y_preS = y((Sstart - 100):(Sstart - 20));
+%         
         X_posS(c,tr,:) = x_posS;
         X_preS(c,tr,:) = x_preS;
+        V_posS(c,tr,:) = v_posS;
+        V_preS(c,tr,:) = v_preS;
         
-        Y_posS(c,tr,:) = y_posS;
-        Y_preS(c,tr,:) = y_preS;
+%         Y_posS(c,tr,:) = y_posS;
+%         Y_preS(c,tr,:) = y_preS;
         
-        clear x x_posS x_preS y y_posS y_preS Send Sstart
+        clear x x_posS x_preS v_posS v_preS Send Sstart %y y_posS y_preS 
     end 
 end
 
@@ -50,39 +60,160 @@ x5 = squeeze(X_posS(5,:,:))';
 y5 = squeeze(Y_posS(5,:,:))';
 x6 = squeeze(X_posS(6,:,:))';
 y6 = squeeze(Y_posS(6,:,:))';
+x7 = squeeze(X_posS(7,:,:))';
+y7 = squeeze(Y_posS(7,:,:))';
+x8 = squeeze(X_posS(8,:,:))';
+y8 = squeeze(Y_posS(8,:,:))';
+
 
 %% overlap intials
 
-x = [x3(1,:),x4(1,:)];
+% X
+x = [x1(1,:),x2(1,:),-x3(1,:),-x4(1,:),x5(1,:),-x6(1,:),x7(1,:),-x8(1,:)];
 xinitmin = min(x);
 x = x - min(x);
-x3init = x(1:60);
-x4init = x(61:120);
+x1init = x(1:80);
+x2init = x(81:160);
+x3init = x(161:240);
+x4init = x(241:320);
+x5init = x(321:400);
+x6init = x(401:480);
+x7init = x(481:560);
+x8init = x(561:640);
+
+
+x1initall = repmat(x1init,61,1);
+x2initall = repmat(x2init,61,1);
 x3initall = repmat(x3init,61,1);
 x4initall = repmat(x4init,61,1);
+x5initall = repmat(x5init,61,1);
+x6initall = repmat(x6init,61,1);
+x7initall = repmat(x7init,61,1);
+x8initall = repmat(x8init,61,1);
+
+x1 = x1 - x1initall;
+x2 = x2 - x2initall;
 x3 = x3 - x3initall;
 x4 = x4 - x4initall;
+x5 = x5 - x5initall;
+x6 = x6 - x6initall;
+x7 = x7 - x7initall;
+x8 = x8 - x8initall;
 
-
-y = [y3(1,:),y4(1,:)];
+% Y
+y = [y1(1,:),y2(1,:),-y3(1,:),-y4(1,:),y5(1,:),-y6(1,:),y7(1,:),-y8(1,:)];
 yinitmin = min(y);
 y = y - min(y);
-y3init = y(1:60);
-y4init = y(61:120);
+y1init = y(1:60);
+y2init = y(61:120);
+y3init = y(121:180);
+y4init = y(181:240);
+y5init = y(241:300);
+y6init = y(301:360);
+y7init = y(361:420);
+y8init = y(421:480);
+
+
+y1initall = repmat(y1init,61,1);
+y2initall = repmat(y2init,61,1);
 y3initall = repmat(y3init,61,1);
 y4initall = repmat(y4init,61,1);
+y5initall = repmat(y5init,61,1);
+y6initall = repmat(y6init,61,1);
+y7initall = repmat(y7init,61,1);
+y8initall = repmat(y8init,61,1);
+
+y1 = y1 - y1initall;
+y2 = y2 - y2initall;
 y3 = y3 - y3initall;
 y4 = y4 - y4initall;
+y5 = y5 - y5initall;
+y6 = y6 - y6initall;
+y7 = y7 - y7initall;
+y8 = y8 - y8initall;
+
+
+
 
 %% plot
 
-figure;plot(x1,'b');hold on;plot(x2,'r');
-figure;plot(x3,y3,'b');hold on;plot(x4,y4,'r');
-figure;plot(x1,'b');hold on;plot(x2,'r');
+figure;plot(x1,'-','color',[0 0 0]);hold on;plot(x2,'-','color',[1 0 0]);
+plot(x5,'-','color',[0 0 1]);plot(x7,'-','color',[0 1 1]);
+
+%% plot 2
+                        
+V_posS(V_posS > 30) = 0;
+V_posS(V_posS < -30) = 0;
+% fitobject = fit([TV(2,:),TV(4,:)]',[V_posS(2,:),-V_posS(4,:)]','smoothingspline','SmoothingParam',.2);
+mdl = LinearModel.fit([TV(2,:),TV(4,:)]',[V_posS(2,:),-V_posS(4,:)]');[vfit2, ~] = predict(mdl,(10:0.5:20)');
+R2 = mdl.Rsquared.Adjusted;
+% vfit2 = feval(fitobject,10:0.5:20);
+% fitobject = fit([TV(1,:),TV(3,:)]',[V_posS(1,:),-V_posS(3,:)]','smoothingspline','SmoothingParam',.2);
+% vfit1 = feval(fitobject,10:0.5:20);
+mdl = LinearModel.fit([TV(1,:),TV(3,:)]',[V_posS(1,:),-V_posS(3,:)]');[vfit1, ~] = predict(mdl,(10:0.5:20)');
+R1 = mdl.Rsquared.Adjusted;
+% fitobject = fit([TV(5,:),TV(6,:)]',[V_posS(5,:),-V_posS(6,:)]','smoothingspline','SmoothingParam',.2);
+% vfit5 = feval(fitobject,10:0.5:20);
+mdl = LinearModel.fit([TV(5,:),TV(6,:)]',[V_posS(5,:),-V_posS(6,:)]');[vfit5, ~] = predict(mdl,(10:0.5:20)');
+R5 = mdl.Rsquared.Adjusted;
+% fitobject = fit([TV(7,:),TV(8,:)]',[V_posS(7,:),-V_posS(8,:)]','smoothingspline','SmoothingParam',.2);
+% vfit7 = feval(fitobject,10:0.5:20);
+mdl = LinearModel.fit([TV(7,:),TV(8,:)]',[V_posS(7,:),-V_posS(8,:)]');[vfit7, ~] = predict(mdl,(10:0.5:20)');
+R7 = mdl.Rsquared.Adjusted;
+
+figure;plot(TV(2,:),V_posS(2,:),'.r','MarkerSize',10);hold on;plot(TV(4,:),-V_posS(4,:),'.r','MarkerSize',10);plot(10:0.5:20,vfit2,'-r','LineWidth',3)
+
+plot(TV(1,:),V_posS(1,:),'.b','MarkerSize',10);hold on;plot(TV(3,:),-V_posS(3,:),'.b','MarkerSize',10);plot(10:0.5:20,vfit1,'-b','LineWidth',3)
+plot(TV(5,:),V_posS(5,:),'.c','MarkerSize',10);hold on;plot(TV(6,:),-V_posS(6,:),'.c','MarkerSize',10);plot(10:0.5:20,vfit5,'-c','LineWidth',3)
+plot(TV(7,:),V_posS(7,:),'.g','MarkerSize',10);hold on;plot(TV(8,:),-V_posS(8,:),'.g','MarkerSize',10);plot(10:0.5:20,vfit7,'-g','LineWidth',3)
+
+% pre- vs post-
+
+% figure;plot(V_preS(2,:),V_posS(2,:),'.r','MarkerSize',10);hold on;plot(-V_preS(4,:),-V_posS(4,:),'.r','MarkerSize',10)
+% 
+% plot(V_preS(1,:),V_posS(1,:),'.b','MarkerSize',10);hold on;plot(-V_preS(3,:),-V_posS(3,:),'.b','MarkerSize',10);
+% plot(V_preS(5,:),V_posS(5,:),'.c','MarkerSize',10);hold on;plot(-V_preS(6,:),-V_posS(6,:),'.c','MarkerSize',10);
+% plot(V_preS(7,:),V_posS(7,:),'.g','MarkerSize',10);hold on;plot(-V_preS(8,:),-V_posS(8,:),'.g','MarkerSize',10);
+
+%% prediction
+
+Ydata1 = [V_posS(2,:),-V_posS(4,:)]';
+Xdata1 = [TV(2,:),TV(4,:)]';
+% Ydata1 = [-V_posS(4,:)]';
+% Xdata1 = [TV(4,:)]';
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\Y1.csv',Ydata1);
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\X1.csv',Xdata1);
+
+Ydata6 = [V_posS(1,:),-V_posS(3,:)]';
+Xdata6 = [TV(1,:),TV(3,:)]';
+% Ydata6 = [-V_posS(3,:)]';
+% Xdata6 = [TV(3,:)]';
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\Y6.csv',Ydata6);
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\X6.csv',Xdata6);
+
+
+Ydata10 = [V_posS(5,:),-V_posS(6,:)]';
+Xdata10 = [TV(5,:),TV(6,:)]';
+% Ydata10 = [-V_posS(6,:)]';
+% Xdata10 = [TV(6,:)]';
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\Y10.csv',Ydata10);
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\X10.csv',Xdata10);
+
+
+Ydata20 = [V_posS(7,:),-V_posS(8,:)]';
+Xdata20 = [TV(7,:),TV(8,:)]';
+% Ydata20 = [-V_posS(8,:)]';
+% Xdata20 = [TV(8,:)]';
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\Y20.csv',Ydata20);
+csvwrite('D:\Project Codes\Behavioral-Normal-Subject\X20.csv',Xdata20);
+
 
 %%
-
-figure( 'Position', [ 100, 380, 1380, 550 ] )
-for i = 1:60
-u(:,i) = TVRegDiff( x1(:,i),500, 1e-6, [], 'small', 1e-8, .001, 1, 1 );
-end
+figure;plot(TV(2,:),V_posS(2,:),'.b','MarkerSize',10);hold on
+plot(TV(1,:),V_posS(1,:),'.b','MarkerSize',10);
+plot(TV(5,:),V_posS(5,:),'.b','MarkerSize',10);
+plot(TV(7,:),V_posS(7,:),'.b','MarkerSize',10);
+plot(TV(4,:),-V_posS(4,:),'.r','MarkerSize',10)
+plot(TV(3,:),-V_posS(3,:),'.r','MarkerSize',10);
+plot(TV(6,:),-V_posS(6,:),'.r','MarkerSize',10);
+plot(TV(8,:),-V_posS(8,:),'.r','MarkerSize',10);
---
+++ plot_regression.py
@@ -0,0 +1,356 @@
+"""
+============================
+Nearest Neighbors regression
+============================
+
+Demonstrate the resolution of a regression problem
+using a k-Nearest Neighbor and the interpolation of the
+target using both barycenter and constant weights.
+
+"""
+print(__doc__)
+
+# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#         Fabian Pedregosa <fabian.pedregosa@inria.fr>
+#
+# License: BSD 3 clause (C) INRIA
+
+
+###############################################################################
+# Generate sample data
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import neighbors
+from numpy import genfromtxt
+from sklearn.cross_validation import KFold
+from sklearn import linear_model
+
+#np.random.seed(0)
+#X = np.sort(5 * np.random.rand(40, 1), axis=0)
+#T = np.linspace(0, 5, 500)[:, np.newaxis]
+#y = np.sin(X).ravel()
+#
+## Add noise to targets
+#y[::5] += 1 * (0.5 - np.random.rand(8))
+
+###############################################################################
+# Load data
+Xdata1 = genfromtxt('X1.csv', delimiter=',')
+Xd1 = np.matrix(Xdata1).reshape(-1,1)
+Ydata1 = genfromtxt('Y1.csv', delimiter=',')
+Yd1 = np.matrix(Ydata1).reshape(-1,1)
+
+Xdata6 = genfromtxt('X6.csv', delimiter=',')
+Xd6 = np.matrix(Xdata6).reshape(-1,1)
+Ydata6 = genfromtxt('Y6.csv', delimiter=',')
+Yd6 = np.matrix(Ydata6).reshape(-1,1)
+
+Xdata10 = genfromtxt('X10.csv', delimiter=',')
+Xd10 = np.matrix(Xdata10).reshape(-1,1)
+Ydata10 = genfromtxt('Y10.csv', delimiter=',')
+Yd10 = np.matrix(Ydata10).reshape(-1,1)
+
+Xdata20 = genfromtxt('X20.csv', delimiter=',')
+Xd20 = np.matrix(Xdata20).reshape(-1,1)
+Ydata20 = genfromtxt('Y20.csv', delimiter=',')
+Yd20 = np.matrix(Ydata20).reshape(-1,1)
+
+
+T = np.linspace(10, 20, 20)[:, np.newaxis];
+
+###############################################################################
+# Fit regression model
+n_neighbors = 10
+knn = neighbors.KNeighborsRegressor(n_neighbors, weights='uniform')
+y1_ = knn.fit(Xd1, Yd1).predict(T)
+
+y6_ = knn.fit(Xd6, Yd6).predict(T)
+
+y10_ = knn.fit(Xd10, Yd10).predict(T)
+
+y20_ = knn.fit(Xd20, Yd20).predict(T)
+
+fig1 = plt.figure()
+plt.scatter(Xd1, Yd1, c='k', label='data')
+line_1, = plt.plot(T, y1_, 'k', label='1 degree')
+legends = plt.legend(handles=[line_1], loc=2)
+ax = plt.gca().add_artist(legends)
+plt.axis([9,21,-5,20])
+plt.title("KNeighborsRegressor (k = %i)" % (n_neighbors))
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+plt.show()
+
+fig2 = plt.figure()
+plt.scatter(Xd6, Yd6, c='b', label='data')
+line_6, = plt.plot(T, y6_, 'b', label='6 degrees')
+legends = plt.legend(handles=[line_6], loc=2)
+ax = plt.gca().add_artist(legends)
+plt.axis([9,21,-5,20])
+plt.title("KNeighborsRegressor (k = %i)" % (n_neighbors))
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+plt.show()
+
+fig3 = plt.figure()
+plt.scatter(Xd10, Yd10, c='g', label='data')
+line_10, = plt.plot(T, y10_, 'g', label='10 degrees')
+legends = plt.legend(handles=[line_10], loc=2)
+ax = plt.gca().add_artist(legends)
+plt.axis([9,21,-5,20])
+plt.title("KNeighborsRegressor (k = %i)" % (n_neighbors))
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+plt.show()
+
+fig4 = plt.figure()
+plt.scatter(Xd20, Yd20, c='r', label='data')
+line_20, = plt.plot(T, y20_, 'r', label='20 degrees')
+legends = plt.legend(handles=[line_20], loc=2)
+ax = plt.gca().add_artist(legends)
+plt.axis([9,21,-5,20])
+plt.title("KNeighborsRegressor (k = %i)" % (n_neighbors))
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+plt.show()
+
+
+
+###############################################################################
+# knearest regression x-validation
+n_neighbors = 10
+kf = KFold(80, n_folds=80)
+Error = np.empty([80,1])
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd1[train], Xd1[test], Yd1[train], Yd1[test]
+    
+    knn = neighbors.KNeighborsRegressor(n_neighbors, weights='distance')
+    y1_ = knn.fit(X_train, y_train).predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    i += 1
+    
+Error1 = np.sqrt(np.mean(Error))
+
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd6[train], Xd6[test], Yd6[train], Yd6[test]
+    
+    knn = neighbors.KNeighborsRegressor(n_neighbors, weights='distance')
+    y1_ = knn.fit(X_train, y_train).predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    i += 1
+    
+Error6 = np.sqrt(np.mean(Error))
+
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd10[train], Xd10[test], Yd10[train], Yd10[test]
+    
+    knn = neighbors.KNeighborsRegressor(n_neighbors, weights='distance')
+    y1_ = knn.fit(X_train, y_train).predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    i += 1
+    
+Error10 = np.sqrt(np.mean(Error))
+
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd20[train], Xd20[test], Yd20[train], Yd20[test]
+    
+    knn = neighbors.KNeighborsRegressor(n_neighbors, weights='distance')
+    y1_ = knn.fit(X_train, y_train).predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    i += 1
+    
+Error20 = np.sqrt(np.mean(Error))
+fig = plt.figure()
+plt.plot([1,6,10,20], [Error1,Error6,Error10,Error20], '-k', [1,6,10,20], [Error1,Error6,Error10,Error20], 'ob')
+plt.xlabel('Dots Diameter');plt.ylabel('X-validation MSE');plt.title('KNeighborsRegressor')
+plt.show()
+
+###############################################################################
+# Bayesian Regression
+clf = linear_model.BayesianRidge(compute_score=True)
+y1_ = clf.fit(Xd1, Yd1).predict(T)
+c1 = clf.coef_
+a1 = clf.alpha_
+l1 = clf.lambda_
+
+y6_ = clf.fit(Xd6, Yd6).predict(T)
+c6 = clf.coef_
+a6 = clf.alpha_
+l6 = clf.lambda_
+
+y10_ = clf.fit(Xd10, Yd10).predict(T)
+c10 = clf.coef_
+a10 = clf.alpha_
+l10 = clf.lambda_
+
+y20_ = clf.fit(Xd20, Yd20).predict(T)
+c20 = clf.coef_
+a20 = clf.alpha_
+l20 = clf.lambda_
+
+fig1 = plt.figure()
+plt.scatter(Xd1, Yd1, c='k', label='data')
+line_1, = plt.plot(T, y1_, 'k', label='1 degree')
+plt.text(10, 18, r'$p(V_e|V_t,\omega,\alpha) = \mathcal{N}(V_e|V_t\omega,\alpha)$')
+plt.text(10, 17, r'$p(\omega|\lambda) = \mathcal{N}(\omega|0,\lambda^{-1})$')
+plt.text(10, 16, r'$\omega = %.3f$' % (c1))
+plt.text(10, 15, r'$\alpha = %.3f$' % (a1))
+plt.text(10, 14, r'$\lambda = %.3f$' % (l1))
+plt.axis([9,21,-5,20])
+plt.title("Bayesian Regression - d = 1 degree")
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+
+plt.show()
+
+fig2 = plt.figure()
+plt.scatter(Xd6, Yd6, c='b', label='data')
+line_1, = plt.plot(T, y6_, 'k', label='1 degree')
+plt.text(10, 18, r'$p(V_e|V_t,\omega,\alpha) = \mathcal{N}(V_e|V_t\omega,\alpha)$')
+plt.text(10, 17, r'$p(\omega|\lambda) = \mathcal{N}(\omega|0,\lambda^{-1})$')
+plt.text(10, 16, r'$\omega = %.3f$' % (c6))
+plt.text(10, 15, r'$\alpha = %.3f$' % (a6))
+plt.text(10, 14, r'$\lambda = %.3f$' % (l6))
+plt.axis([9,21,-5,20])
+plt.title("Bayesian Regression - 6 degrees")
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+plt.show()
+
+fig3 = plt.figure()
+plt.scatter(Xd10, Yd10, c='g', label='data')
+line_1, = plt.plot(T, y10_, 'k', label='1 degree')
+plt.text(10, 18, r'$p(V_e|V_t,\omega,\alpha) = \mathcal{N}(V_e|V_t\omega,\alpha)$')
+plt.text(10, 17, r'$p(\omega|\lambda) = \mathcal{N}(\omega|0,\lambda^{-1})$')
+plt.text(10, 16, r'$\omega = %.3f$' % (c10))
+plt.text(10, 15, r'$\alpha = %.3f$' % (a10))
+plt.text(10, 14, r'$\lambda = %.3f$' % (l10))
+plt.axis([9,21,-5,20])
+plt.title("Bayesian Regression - d = 10 degrees")
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+plt.show()
+
+fig4 = plt.figure()
+plt.scatter(Xd20, Yd20, c='r', label='data')
+line_1, = plt.plot(T, y20_, 'k', label='1 degree')
+plt.text(10, 18, r'$p(V_e|V_t,\omega,\alpha) = \mathcal{N}(V_e|V_t\omega,\alpha)$')
+plt.text(10, 17, r'$p(\omega|\lambda) = \mathcal{N}(\omega|0,\lambda^{-1})$')
+plt.text(10, 16, r'$\omega = %.3f$' % (c20))
+plt.text(10, 15, r'$\alpha = %.3f$' % (a20))
+plt.text(10, 14, r'$\lambda = %.3f$' % (l20))
+plt.axis([9,21,-5,20])
+plt.title("Bayesian Regression - d = 20 degrees")
+plt.xlabel('Target Velocity (degree/s)')
+plt.ylabel('Eye Velocity (degree/s)')
+plt.show()
+
+###############################################################################
+# Bayesian Regression x-validation
+
+clf = linear_model.BayesianRidge(compute_score=True)
+kf = KFold(160, n_folds=160)
+c = np.empty([160,1])
+a = np.empty([160,1])
+l = np.empty([160,1])
+Error = np.empty([160,1])
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd1[train], Xd1[test], Yd1[train], Yd1[test]
+    clf.fit(X_train, y_train)
+    y1_ = clf.predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    c[i] = clf.coef_
+    a[i] = clf.alpha_
+    l[i] = clf.lambda_
+    i += 1
+    
+Error1 = np.sqrt(np.mean(Error))
+c1 = np.sqrt(np.mean(c))
+a1 = np.sqrt(np.mean(a))
+l1 = np.sqrt(np.mean(l))
+
+
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd6[train], Xd6[test], Yd6[train], Yd6[test]
+    clf.fit(X_train, y_train)
+    y1_ = clf.predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    c[i] = clf.coef_
+    a[i] = clf.alpha_
+    l[i] = clf.lambda_
+    i += 1
+    
+Error6 = np.sqrt(np.mean(Error))
+c6 = np.sqrt(np.mean(c))
+a6 = np.sqrt(np.mean(a))
+l6 = np.sqrt(np.mean(l))
+
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd10[train], Xd10[test], Yd10[train], Yd10[test]
+    clf.fit(X_train, y_train)
+    y1_ = clf.predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    c[i] = clf.coef_
+    a[i] = clf.alpha_
+    l[i] = clf.lambda_
+    i += 1
+    
+Error10 = np.sqrt(np.mean(Error))
+c10 = np.sqrt(np.mean(c))
+a10 = np.sqrt(np.mean(a))
+l10 = np.sqrt(np.mean(l))
+
+Error = np.empty([160,1])
+i = 0
+for train, test in kf:
+    
+    X_train, X_test, y_train, y_test = Xd20[train], Xd20[test], Yd20[train], Yd20[test]
+    clf.fit(X_train, y_train)
+    y1_ = clf.predict(X_test)
+    Error[i] = (y1_-y_test)**2
+    c[i] = clf.coef_
+    a[i] = clf.alpha_
+    l[i] = clf.lambda_
+    i += 1
+    
+Error20 = np.sqrt(np.mean(Error))
+c20 = np.sqrt(np.mean(c))
+a20 = np.sqrt(np.mean(a))
+l20 = np.sqrt(np.mean(l))
+
+fig = plt.figure()
+plt.plot([1,6,10,20], [Error1,Error6,Error10,Error20], '-k', [1,6,10,20], [Error1,Error6,Error10,Error20], 'ob')
+plt.xlabel('Dots Diameter');plt.ylabel('X-validation MSE');plt.title('Bayesian Regression')
+plt.show()
+
+fig2 = plt.figure()
+plt.plot([1,6,10,20], [c1,c6,c10,c20], '-k', [1,6,10,20], [c1,c6,c10,c20], 'ob')
+plt.xlabel('Dots Diameter');plt.ylabel('coefficient');plt.title('Bayesian Regression')
+plt.grid(True)
+plt.show()
+
+fig3 = plt.figure()
+plt.plot([1,6,10,20], [a1,a6,a10,a20], '-k', [1,6,10,20], [a1,a6,a10,a20], 'ob')
+plt.xlabel('Dots Diameter');plt.ylabel('alpha');plt.title('Bayesian Regression')
+plt.grid(True)
+plt.show()
+
+fig4 = plt.figure()
+plt.plot([1,6,10,20], [l1,l6,l10,l20], '-k', [1,6,10,20], [l1,l6,l10,l20], 'ob')
+plt.xlabel('Dots Diameter');plt.ylabel('lambda');plt.title('Bayesian Regression')
+plt.grid(True)
+plt.show()
